41c57517988966c0e8254130f60859d3ca2ffa3e
zubasyan <mikoraio914@icloud.com>
2024-11-29 16:29:38 +0900

    add cumulative_sum cpp,hpp

--- dp/cumlative-sum.hpp
+++ dp/cumlative-sum.hpp
@@ -1,44 +1,44 @@
-template<class T>
-struct Cumulative_sum{
-public:
-    Cumulative_sum()=default;
-    explicit Cumulative_sum(int n){
-        _n=n+1;
-        _data.resize(_n,static_cast<T>(0));
+template <class T>
+struct Cumulative_sum {
+ public:
+  Cumulative_sum() = default;
+  explicit Cumulative_sum(int n) {
+    _n = n + 1;
+    _data.resize(_n, static_cast<T>(0));
+  }
+  explicit Cumulative_sum(const vector<T>& a) {
+    _n = static_cast<int>(a.size() + 1);
+    _data.push_back(static_cast<T>(0));
+    for (int i = 0; i < static_cast<T>(a.size()); i++) {
+      _data.push_back(a[i]);
     }
-    explicit Cumulative_sum(const vector<T>& a){
-        _n=static_cast<int>(a.size()+1);
-        _data.push_back(static_cast<T>(0));
-        for(int i=0;i<static_cast<T>(a.size());i++){
-            _data.push_back(a[i]);
-        }
-    }
+  }
 
-    void set(int p,T x){
-        assert(0<=p&&p+1<_n);
-        _data[p+1]=x;
-    }
-    
-    void build(){
-        for(int i=1;i<_n;i++){
-            _data[i]+=_data[i-1];
-        }
-    }
+  void set(int p, T x) {
+    assert(0 <= p && p + 1 < _n);
+    _data[p + 1] = x;
+  }
 
-    T sum(int l,int r){
-        assert(0<=l&&l<_n);
-        assert(0<=r&&r<_n);
-        return _data[r]-_data[l];
+  void build() {
+    for (int i = 1; i < _n; i++) {
+      _data[i] += _data[i - 1];
     }
+  }
 
-    void show(){
-        for(int i=1;i<_n;i++){
-            cout<<_data[i]<<" ";
-        }
-        cout<<endl;
+  T sum(int l, int r) {
+    assert(0 <= l && l < _n);
+    assert(0 <= r && r < _n);
+    return _data[r] - _data[l];
+  }
+
+  void show() {
+    for (int i = 1; i < _n; i++) {
+      cout << _data[i] << " ";
     }
+    cout << endl;
+  }
 
-private:
-    int _n;
-    vector<T> _data;
+ private:
+  int _n;
+  vector<T> _data;
 };
